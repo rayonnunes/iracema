c987054e7de194b4d1924356f1f9543bef7c0e26
Rayon Lindraz <rayonnunes@hotmail.com>
2023-04-03 01:17:05 -0300

    fixed d.ts file declarations

--- packages/button/vite.config.ts
+++ packages/button/vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import typescript from '@rollup/plugin-typescript';
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
     react(),
+    dts(),
   ],
   build: {
     target: 'es2015',
     lib: {
       entry: 'src/index.ts',
       formats: ['es', 'cjs'],
+      fileName: "index",
       name: 'button',
     },
     rollupOptions: {
@@ -21,14 +23,6 @@ export default defineConfig({
           'react-dom': 'ReactDOM',
         },
       },
-      plugins: [
-        typescript({
-          declaration: true,
-          declarationDir: './dist/types',
-          rootDir: 'src',
-          exclude: ['**/__tests__/*', '**/*.test.ts', '**/*.test.tsx']
-        })
-      ]
     },
   },
 });
