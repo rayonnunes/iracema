74344e906bf3428b31ce7af1773840aade8c1f88
Rayon Lindraz <rayonnunes@hotmail.com>
2023-10-09 22:50:36 -0300

    update storybook

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,15 +1,31 @@
+import { dirname, join } from "path";
 module.exports = {
   "features": {
     "postcss": false,
   },
+
   "stories": [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-mdx-gfm")
   ],
-  "framework": "@storybook/react"
+
+  "framework": {
+    name: getAbsolutePath("@storybook/react-webpack5"),
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
+}
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
 }
